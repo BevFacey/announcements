e7e4287e479e1a47f884e37bf266fca3283cb1ba
David Hay <misterhay@gmail.com>
2025-03-06 21:30:22 -0700

    Rename upload_video

--- scripts/main.py
+++ scripts/main.py
@@ -3,7 +3,7 @@ from datetime import date
 from generate_audio import generate_audio
 from download_slides import download_and_convert_slides
 from create_video import create_video
-from upload_youtube import upload_video
+from upload_video import upload_video
 
 TEMP_DIR = 'temporary_directory'
 SHEET_KEY = '1SV4eWy58Y6iEIIOGUH60xUZV59eaY7ewp4EoeCZzNxc'
@@ -18,7 +18,7 @@ date_today_iso = date.today().isoformat()
 video_output_file = f"Bev Facey Announcements {date_today_iso} (AI Generated).mp4"
 create_video(slide_files, audio_files, TEMP_DIR, video_output_file)
 
-upload_video(video_output_file)
+upload_video()
 
 os.rmdir(TEMP_DIR)
 print('Process completed successfully.')
