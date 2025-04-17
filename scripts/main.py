7a21abaf8a21ce6d16e2efcca5d8c09c54d38d47
David Hay <misterhay@gmail.com>
2025-04-17 07:44:46 -0600

    Switch to Polly, double update sheet

--- scripts/main.py
+++ scripts/main.py
@@ -2,15 +2,17 @@ import requests
 from time import sleep
 from download_text import download_text
 from download_slides import download_slides
-from create_audio import create_audio
-#from create_audio_amazon import create_audio
+#from create_audio import create_audio
+from create_audio_amazon import create_audio
 from create_video import create_video
 from upload_video import upload_video
 
 # update the spreadsheet and slides online
 r = requests.get('https://script.google.com/macros/s/AKfycbyn5y-tbC8DYHpahMnrFgE_KPxuG-M2Q5f5N7IhfRWUevl5akfLNWSFVZsIeptXTMCt/exec')
+sleep(3)
+r = requests.get('https://script.google.com/macros/s/AKfycbyn5y-tbC8DYHpahMnrFgE_KPxuG-M2Q5f5N7IhfRWUevl5akfLNWSFVZsIeptXTMCt/exec')
 if r.status_code == 200:
-    sleep(3) # wait for the slides to be updated
+    sleep(5) # wait for the slides to be updated
     download_text()
     download_slides()
     create_audio()
